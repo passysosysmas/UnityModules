a5e7929208774814b6d40a486e8c41b11a6c676f
jselstad <jselstad@leapmotion.com>
2016-06-10 14:00:30 -0700

    Testing Dumb Grabbing Paradigm
    If we have oversensitive grab detectors, what if we only trigger if they
    all agree?
    
    And if release is under sensitive, release when only one says to
    release?

---
+++ Assets/LeapMotionModules/PuppetHand/DistanceFields/SphereGrab.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+namespace Leap.Unity {
+  public class SphereGrab : MonoBehaviour {
+    public Leap.Unity.GrabDetector PinchDetector;
+
+    public Leap.Unity.IHandModel _handModel;
+
+    Collider[] colliders;
+    Hand leapHand;
+    GameObject Marker;
+
+    [SerializeField]
+    private Mesh _sphereMesh;
+    [SerializeField]
+    private Material _sphereMaterial;
+
+    void Start() {
+
+      Marker = new GameObject("Marker");
+      Marker.transform.localPosition = Vector3.zero;
+      Marker.transform.localRotation = Quaternion.identity;
+      Marker.transform.localScale = Vector3.one * 0.06f;
+      Marker.AddComponent<MeshFilter>().mesh = _sphereMesh;
+      Marker.AddComponent<MeshRenderer>().sharedMaterial = _sphereMaterial;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+      if (PinchDetector.DidStartPinch) {
+        leapHand = _handModel.GetLeapHand();
+        colliders = Physics.OverlapSphere((leapHand.Fingers[2].Bone(Bone.BoneType.TYPE_PROXIMAL).PrevJoint + (leapHand.PalmNormal * 0.04f)).ToVector3(), 0.03f);
+        Marker.transform.position = (leapHand.Fingers[2].Bone(Bone.BoneType.TYPE_PROXIMAL).PrevJoint + (leapHand.PalmNormal * 0.04f)).ToVector3();
+        foreach (Collider col in colliders) {
+          col.gameObject.GetComponent<Renderer>().material.color = Color.green;
+          col.transform.parent = PinchDetector.transform;
+        }
+        Marker.GetComponent<Renderer>().material.color = Color.green;
+      } else if (PinchDetector.DidEndPinch) {
+        foreach (Collider col in colliders) {
+          col.gameObject.GetComponent<Renderer>().material.color = Color.white;
+          col.transform.parent = null;
+        }
+        Marker.GetComponent<Renderer>().material.color = Color.white;
+      }
+    }
+  }
+}
