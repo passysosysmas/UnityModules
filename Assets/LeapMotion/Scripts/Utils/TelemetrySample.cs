2813e6936a0a43316d962fadc416dae5dc1bb05a
Amarcolina <Amarcoli@ucsc.edu>
2016-10-18 14:12:44 -0700

    Re-added logic for telemetry class

--- Assets/LeapMotion/Scripts/Utils/TelemetrySample.cs
+++ Assets/LeapMotion/Scripts/Utils/TelemetrySample.cs
@@ -4,7 +4,7 @@ using System.Threading;
 namespace Leap.Unity {
 
   public class Telemetry : IDisposable {
-    private static int _nestingLevel = 0;
+    private static uint _nestingLevel = 0;
 
     private Controller _controller;
     private string _filename;
@@ -26,26 +26,24 @@ namespace Leap.Unity {
         return null;
       } else {
         _nestingLevel++;
-        //_start = provider.GetLeapController().TelemetryGetNow();
-        _start = 0;
+        _start = _controller.TelemetryGetNow();
         _lineNumber = lineNumber;
         _zoneName = zoneName;
+        return this;
       }
     }
 
     public void Dispose() {
-      /*
       _nestingLevel--;
 
-      ulong end = _provider.GetLeapController().TelemetryGetNow();
-      _provider.GetLeapController().TelemetryProfiling((uint)Thread.CurrentThread.ManagedThreadId,
-                                                       _start,
-                                                       end,
-                                                       _nestingLevel,
-                                                       _filename,
-                                                       _lineNumber,
-                                                       _zoneName);
-                                                       */
+      ulong end = _controller.TelemetryGetNow();
+      _controller.TelemetryProfiling((uint)Thread.CurrentThread.ManagedThreadId,
+                                     _start,
+                                     end,
+                                     _nestingLevel,
+                                     _filename,
+                                     _lineNumber,
+                                     _zoneName);
     }
   }
 }
