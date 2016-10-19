2d8556290832708097685fe0e57c935d927fe2e9
Amarcolina <Amarcoli@ucsc.edu>
2016-10-18 17:57:36 -0700

    Changed name

--- Assets/LeapMotion/Scripts/Utils/BasicTelemetry.cs
+++ Assets/LeapMotion/Scripts/Utils/BasicTelemetry.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using Leap.Unity;
 
-public class EndOfFrameWaiter : MonoBehaviour {
+public class BasicTelemetry : MonoBehaviour {
 
   [SerializeField]
   private LeapServiceProvider _provider;
