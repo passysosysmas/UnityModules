cea5acb72a34500096659b2f72a9c1cbe9ef74ed
Amarcolina <Amarcoli@ucsc.edu>
2016-10-18 17:18:05 -0700

    Added basic telemetry behaviour

---
+++ Assets/LeapMotion/Scripts/Utils/BasicTelemetry.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class EndOfFrameWaiter : MonoBehaviour {
+
+  [SerializeField]
+  private LeapServiceProvider _provider;
+
+  private Telemetry _telemetry;
+
+  IEnumerator Start() {
+    _telemetry = new Telemetry(_provider, "Frame");
+
+    WaitForEndOfFrame waiter = new WaitForEndOfFrame();
+    while (true) {
+      var sample = _telemetry.Sample(17, "Frame");
+      yield return waiter;
+      sample.Dispose();
+    }
+  }
+
+  void OnEnable() {
+    Camera.onPreRender += onPreRender;
+    Camera.onPostRender += onPostRender;
+  }
+
+  void OnDisable() {
+    Camera.onPreRender -= onPreRender;
+    Camera.onPostRender -= onPostRender;
+  }
+
+  private Telemetry.TelemetrySample _cameraSample;
+
+  private void onPreRender(Camera c) {
+    _cameraSample = _telemetry.Sample(36, "Render Camera " + c.name);
+  }
+
+  private void onPostRender(Camera c) {
+    _cameraSample.Dispose();
+  }
+}
