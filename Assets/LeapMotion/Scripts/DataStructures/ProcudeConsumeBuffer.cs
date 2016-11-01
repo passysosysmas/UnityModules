562ee6d34d2880ece2e105d4988557e37528f3bd
Amarcolina <Amarcoli@ucsc.edu>
2016-11-01 12:31:58 -0700

    Added namespace

--- Assets/LeapMotion/Scripts/DataStructures/ProcudeConsumeBuffer.cs
+++ Assets/LeapMotion/Scripts/DataStructures/ProcudeConsumeBuffer.cs
@@ -1,56 +1,59 @@
 ﻿
-public class ProduceConsumeBuffer<T> {
-  private T[] _buffer;
-  private uint _bufferMask;
-  private uint _head, _tail;
+namespace Leap.Unity {
 
-  /// <summary>
-  /// Constructs a new produce consumer buffer of a given capacity.  This capacity
-  /// is fixed and cannot be changed after the buffer is created.
-  /// </summary>
-  public ProduceConsumeBuffer(int capacity) {
-    _buffer = new T[capacity];
-    _bufferMask = (uint)(capacity - 1);
-    _head = 0;
-    _tail = 0;
-  }
+  public class ProduceConsumeBuffer<T> {
+    private T[] _buffer;
+    private uint _bufferMask;
+    private uint _head, _tail;
 
-  /// <summary>
-  /// Returns the maximum number of elements that the buffer can hold.
-  /// </summary>
-  public int Capacity {
-    get {
-      return _buffer.Length;
-    }
-  }
-
-  /// <summary>
-  /// Tries to push a value into the buffer.  If the buffer is already full, this
-  /// method will perform no action and return false.  This method is only safe to
-  /// be called from a single producer thread.
-  /// </summary>
-  public bool TryPush(ref T t) {
-    uint nextTail = (_tail + 1) & _bufferMask;
-    if (nextTail == _head) return false;
-
-    _buffer[_tail] = t;
-    _tail = nextTail;
-    return true;
-  }
-
-  /// <summary>
-  /// Tries to pop a value off of the buffer.  If the buffer is empty this method
-  /// will perform no action and return false.  This method is only safe to be
-  /// called from a single consumer thread.
-  /// </summary>
-  public bool TryPop(out T t) {
-    if (_tail == _head) {
-      t = default(T);
-      return false;
+    /// <summary>
+    /// Constructs a new produce consumer buffer of a given capacity.  This capacity
+    /// is fixed and cannot be changed after the buffer is created.
+    /// </summary>
+    public ProduceConsumeBuffer(int capacity) {
+      _buffer = new T[capacity];
+      _bufferMask = (uint)(capacity - 1);
+      _head = 0;
+      _tail = 0;
     }
 
-    t = _buffer[_head];
-    _head = (_head + 1) & _bufferMask;
-    return true;
+    /// <summary>
+    /// Returns the maximum number of elements that the buffer can hold.
+    /// </summary>
+    public int Capacity {
+      get {
+        return _buffer.Length;
+      }
+    }
+
+    /// <summary>
+    /// Tries to push a value into the buffer.  If the buffer is already full, this
+    /// method will perform no action and return false.  This method is only safe to
+    /// be called from a single producer thread.
+    /// </summary>
+    public bool TryPush(ref T t) {
+      uint nextTail = (_tail + 1) & _bufferMask;
+      if (nextTail == _head) return false;
+
+      _buffer[_tail] = t;
+      _tail = nextTail;
+      return true;
+    }
+
+    /// <summary>
+    /// Tries to pop a value off of the buffer.  If the buffer is empty this method
+    /// will perform no action and return false.  This method is only safe to be
+    /// called from a single consumer thread.
+    /// </summary>
+    public bool TryPop(out T t) {
+      if (_tail == _head) {
+        t = default(T);
+        return false;
+      }
+
+      t = _buffer[_head];
+      _head = (_head + 1) & _bufferMask;
+      return true;
+    }
   }
 }
