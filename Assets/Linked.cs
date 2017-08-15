5ee769188d90ace78854d2eaec90bd9bb9ab766c
Amarcolina <Amarcolina@gmail.com>
2017-08-14 18:33:40 -0700

    Pulled over linked prop changes

---
+++ Assets/Linked.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace Leap.Unity {
+
+  [Serializable]
+  public class LinkedInt : Linked<int> { }
+
+  [Serializable]
+  public class LinkedFloat : Linked<float> { }
+
+  [Serializable]
+  public class LinkedVector2 : Linked<Vector2> { }
+
+  [Serializable]
+  public class LinkedVector3 : Linked<Vector3> { }
+
+  [Serializable]
+  public class LinkedVector4 : Linked<Vector4> { }
+
+  [Serializable]
+  public class LinkedQuaternion : Linked<Quaternion> { }
+
+  [Serializable]
+  public class LinkedColor : Linked<Color> { }
+
+  [Serializable]
+  public abstract class LinkedBase {
+    [SerializeField]
+    protected UnityObject _reference;
+
+    [SerializeField]
+    protected string _propertyName;
+
+    protected FieldInfo _fieldInfo;
+
+    protected void setup() {
+      if (_reference == null) {
+        throw new NullReferenceException("The linked object is null or has been destroyed!");
+      }
+
+      if (_fieldInfo == null) {
+        _fieldInfo = _reference.GetType().
+                                GetField(_propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+      }
+    }
+  }
+
+  [Serializable]
+  public class Linked<T> : LinkedBase {
+    public T value {
+      get {
+        setup();
+
+        return (T)_fieldInfo.GetValue(_reference);
+      }
+      set {
+        setup();
+
+        _fieldInfo.SetValue(_reference, value);
+      }
+    }
+  }
+}
---
+++ Assets/LinkedPropertyDrawer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Leap.Unity {
+
+  [CustomPropertyDrawer(typeof(LinkedBase), useForChildren: true)]
+  public class LinkedPropertyDrawer : PropertyDrawer {
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+      SerializedProperty objProp = property.FindPropertyRelative("_reference");
+      SerializedProperty nameProp = property.FindPropertyRelative("_propertyName");
+
+      Rect top = position;
+      top.height = EditorGUIUtility.singleLineHeight;
+
+      Rect bottom = top;
+      bottom.y += EditorGUIUtility.singleLineHeight;
+
+      Rect bottomLeft, bottomRight;
+      bottom.SplitHorizontallyWithRight(out bottomLeft, out bottomRight, EditorGUIUtility.singleLineHeight * 1.1f);
+
+      EditorGUI.PropertyField(top, objProp, label);
+
+      EditorGUI.BeginDisabledGroup(true);
+      EditorGUI.PropertyField(bottomLeft, nameProp, new GUIContent(" "));
+      EditorGUI.EndDisabledGroup();
+
+      if (GUI.Button(bottomRight, "", EditorStyles.foldout)) {
+        GenericMenu menu = new GenericMenu();
+        //loop through all that shit
+      }
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+      return EditorGUIUtility.singleLineHeight * 2;
+    }
+
+  }
+}
